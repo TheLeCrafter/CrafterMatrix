02d40a0c5a34bcd1ae6d80a939591ebe0a39da5c
TheLeCrafter <thelecrafter@proton.me>
2023-05-07 11:30:33 +0200

    Set explicit api for Radians.kt

--- src/main/kotlin/dev/thelecrafter/matrix/Radians.kt
+++ src/main/kotlin/dev/thelecrafter/matrix/Radians.kt
@@ -1,14 +1,14 @@
 package dev.thelecrafter.matrix
 
-class Radians(val value: Double) {
+public class Radians(public val value: Double) {
 
-    companion object {
-        fun fromDegree(degree: Double): Radians {
+    public companion object {
+        public fun fromDegree(degree: Double): Radians {
             return Radians(degree * Math.PI / 180)
         }
     }
 
-    fun toDegree(): Double {
+    public fun toDegree(): Double {
         return value * 180 / Math.PI
     }
 
